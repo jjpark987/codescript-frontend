52aa445a152cbe2c09cd1d948cb77bc571e1a7f1
Jason Park <jjpark987@gmail.com>
2025-03-04 18:14:23 -0800

    Setup UI and fetch from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import toggleViewIcon from './assets/toggle-view-btn-icon.png'
+import randomProblemIcon from './assets/random-problem-btn-icon.png'
+import submitIcon from './assets/submit-btn-icon.png'
 import './App.css'
 
+const RANDOM_PROBLEM_URL = import.meta.env.VITE_RANDOM_PROBLEM_URL
+
+interface Problem {
+  title: string;
+  difficulty: string;
+  description: string;
+  constraints: string;
+  examples: Example[];
+}
+
+interface Example {
+  input: string;
+  output: string;
+  explanation: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [problem, setProblem] = useState<Problem>({
+    title: '',
+    difficulty: '',
+    description: '',
+    constraints: '',
+    examples: []
+  });
+
+  async function fetchProblem() {
+    try {
+      console.log(RANDOM_PROBLEM_URL)
+      const response = await fetch(RANDOM_PROBLEM_URL);
+      if (!response.ok) throw new Error('❌ App.tsx -> API Error');
+      const problem_data = await response.json();
+      setProblem({
+        title: problem_data.title,
+        difficulty: problem_data.difficulty,
+        description: problem_data.description,
+        constraints: problem_data.constraints,
+        examples: problem_data.examples
+      });
+    } catch (error) {
+      console.error('❌ App.tsx -> Fetch error', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProblem();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div id='app'>
+      <div id='toolbar'>
+        <button>
+          <img src={toggleViewIcon} alt='Toggle View Button Icon' />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button>
+          <img src={randomProblemIcon} alt='Random Problem Button Icon' />
+        </button>
+        <button>
+          <img src={submitIcon} alt='Submit Button Icon' />
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div id='container'>
+        <div id='left'>
+          <div id='problem-title'>{problem.title}</div>
+          <div id='problem-difficulty'>{problem.difficulty}</div>
+          <div>{problem.description}</div>
+          <div>{problem.constraints}</div>
+          <div id='problem-examples-container'>
+            {problem.examples.map((example, index) => (
+              <div key={index}>
+                <div>{example.input}</div>
+                <div>{example.output}</div>
+                <div>{example.explanation}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div id='right'>
+          This is the right side
+        </div>
+      </div>
+    </div>
   )
 }
 
